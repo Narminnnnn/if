f6ab84c51037af1dd491a56d7e804f421b3b858f
fidan86 <ayka-nurka-2015@mail.ru>
2024-10-25 17:59:22 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,74 @@
+// 1)Ədədin müsbət, mənfi və ya sıfır olmasını müəyyən edən
+//  if-else if-else konstruksiyasını yaradın.
+
+// let a = prompt("ededi daxil edin");
+// if (a > 0) {
+//     console.log("eded musbetdir");
+    
+// } else if (a < 0){
+//     console.log("eded menfidir");
+    
+// } else{
+//     console.log("eded 0-a beraberdir");
+    
+// }
+
+
+// 2) Müəyyən bir istifadəçi roluna
+//  (admin, istifadəçi, qonaq) əsaslanan müxtəlif mesajları
+//  qaytaran keçid-case strukturunu yazın.
+// let guess = prompt("Istifadecii rolunu daxil et")
+// if (guess == "Admin") {
+//     console.log("Hormetli Admin, xos gelmisiniz");
+    
+// } else if (guess == "User") {
+//     console.log("Hormetli User, xos gelmisiniz"); 
+// }else{
+//     console.log("Horemetli qonaq, zehmet olmasa qeydiyyatdan kecin");
+    
+// }
+
+// 3) Ədədin cüt və ya tək olduğunu müəyyən edən if-else 
+// konstruksiyasını yazın.
+
+// let number = prompt("ededi daxil edin")
+// if (number %2 === 0) {
+//     console.log("eded cutdur");
+    
+// } else {
+//     console.log("eded tekdir");
+// }
+
+
+// 5) == və === operatorları arasındakı fərqi göstərən numune 
+// yazın.
+
+// let b = 10;
+// let c = "10";
+// console.log(b == c); //true
+// console.log(b === c); //falce
+
+
+
+// 6) !== operatorundan istifadə edərək ədədin 10-a bərabər olmadığını 
+// yoxlayan ifadə yaradın.
+
+// let d = 12;
+// let f = 13;
+
+// let result1 = d+f;
+//  console.log( result1 !== 10);
+
+
+
+
+// ! //  7)Ədədin 5-dən böyük və 10-dan kiçik olmasını yoxlayan ifadə yazın. alinmadi
+// let eded = prompt("ededi daxil edin")
+//  if (eded > 5 && eded < 10) {
+//     console.log("eded 5den boyukdur ve eded 10dan kicikdir");
+    
+//   }else{
+//     console.log("eded 10-dan boyukdur");
+    
+//  }
+ 
